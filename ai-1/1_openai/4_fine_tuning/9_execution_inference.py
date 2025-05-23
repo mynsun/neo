4c85355e2eaf90d2cc964cc701ac50156e7431bd
mynsun <alstjs3022@naver.com>
2025-05-23 07:54:29 +0000

    finetuning

--- ai-1/1_openai/4_fine_tuning/9_execution_inference.py
+++ ai-1/1_openai/4_fine_tuning/9_execution_inference.py
@@ -3,7 +3,7 @@ client = OpenAI()
 
 prompt = '좋아하는 음식은 무엇인가요? -> '
 response = client.chat.completions.create(
-    model = 'ft-xxx',
+    model = 'ft:gpt-3.5-turbo-0125:personal::BaH5ssr3',
     messages = [
         {'role':'user', 'content': prompt}
     ]
