9af3a124774f4d99bac370b743581c412d2a52cd
mynsun <alstjs3022@naver.com>
2025-05-28 08:18:01 +0000

    up

--- ai-2/3_openai/p181_app.py
+++ ai-2/3_openai/p181_app.py
@@ -1,18 +1,27 @@
 import openai
+import requests
 from IPython.display import Image, display
 
 org_img_file = './data/org_image_for_edit.png'
 mask_img_file = './data/mask_image_for_edit.png'
 
-response = openai.Image.create_edit(
-    image = open(org_img_file, 'rb'),
-    mask = open(mask_img_file, 'rb'),
-    prompt="Happy robots swimming in the water",
-    n=1,
-    size="512x512"
-)
 
-image_url = response['data'][0]['url']
-print(image_url)
-display(Image(url=image_url))
-print('-' * 50)
+with open(org_img_file, "rb") as image, open(mask_img_file, "rb") as mask:
+    response = requests.post(
+        "https://api.openai.com/v1/images/edits",
+        headers={
+            "Authorization": f"Bearer {openai.api_key}"
+        },
+        files={
+            "image": ("org_img_file.png", image, "image/png"),
+            "mask": ("mask_img_file.png", mask, "image/png")
+        },
+        data={
+            "prompt": "Happy robots swimming in the water",
+            "n": 1,
+            "size": "512x512"
+        }
+    )
+
+result = response.json()
+print(result["data"][0]["url"])
