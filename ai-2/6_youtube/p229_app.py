9fef982e422f92acd3c6743c352d2dbaca37e4b2
mynsun <alstjs3022@naver.com>
2025-05-29 14:53:49 +0900

    up

--- ai-2/6_youtube/p229_app.py
+++ ai-2/6_youtube/p229_app.py
@@ -2,20 +2,24 @@ import yt_dlp
 
 def get_youtube_video_info(video_url):
     ydl_opts = {
-        'cookies': './data/cookies.txt',
+        'cookies' :'./data/cookies.txt',
         'noplaylist': True,
         'quiet': True,
         'no_warnings': True
-    } 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        video_info = ydl.extract_info(video_url, download=False)
-        video_id = video_info['id']
-        title = video_info['title']
-        upload_data = video_info['upload_data']
-        channel = video_info['channel']
-        duration = video_info['duration_string']
+    }
 
-    return video_id, title, upload_data, channel, duration
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            video_info = ydl.extract_info(video_url, download=False)
+            return {
+                'id' : video_info.get('id'),
+                'title' : video_info.get('title'),
+                'upload_date': video_info.get('upload_date'),
+                'channel' : video_info.get('channel'),
+                'duration' : video_info.get('duration_string'),
+            }
+    except Exception as e:
+        return f'Error: {e}'
 
 video_url = 'https://www.youtube.com/watch?v=pSJrML-TTmI'
 print(get_youtube_video_info(video_url))
